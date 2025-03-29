c468d74abbc98fc1c09819e761cc742d599f9a8e
Dillon Steyl <dillon.steyl@snazzyshrimp.com>
2025-03-29 20:04:00 +1000

    style decay

--- player/score/style_manager.gd
+++ player/score/style_manager.gd
@@ -6,6 +6,8 @@ signal bracket_changed(new_bracket: StyleBracket)
 
 const MIN_STYLE: float = 0.0
 const MAX_STYLE: float = 100.0
+const STYLE_DECAY_DELAY: float = 2.0
+const STYLE_DECAY_PER_SECOND: float = 8.0
 
 
 class StyleBracket:
@@ -36,11 +38,17 @@ class StyleBracket:
 @onready var current_bracket: StyleBracket = style_brackets[0]
 @onready var style: float = 0:
 	set = _set_style
+@onready var style_decay_timer: Timer = Timer.new()
 
 
 func _ready() -> void:
 	style_brackets_descending = style_brackets.duplicate()
 	style_brackets_descending.reverse()
+
+	add_child(style_decay_timer)
+	style_decay_timer.one_shot = true
+	style_decay_timer.wait_time = STYLE_DECAY_DELAY
+	style_decay_timer.start()
 
 
 func get_current_bracket() -> StyleBracket:
@@ -51,7 +59,16 @@ func get_current_bracket() -> StyleBracket:
 	return style_brackets[0]
 
 
+func _physics_process(delta: float) -> void:
+	if style_decay_timer.is_stopped():
+		style -= STYLE_DECAY_PER_SECOND * delta
+
+
 func _set_style(new_style: float):
+	if new_style > style:
+		# style increased - delay the decay
+		style_decay_timer.start()
+
 	style = clamp(new_style, MIN_STYLE, MAX_STYLE)
 	current_bracket = get_current_bracket()
 	style_changed.emit(new_style)
